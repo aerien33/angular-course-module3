2e17a9b75328e0ffbb484fe23b152d0dab3c5caf
Agnieszka <145773068+aerien33@users.noreply.github.com.>
2024-05-13 11:23:49 +0200

    Add service in app.js (MenuSearchService)

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,15 @@
 'use strict';
 
 angular.module('NarrowItDownApp', [])
-.controller('NarrowItDownController', NarrowItDownController);
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService);
 
 function NarrowItDownController() {
     var controller = this;
+}
+
+function MenuSearchService() {
+    var service = this;
 }
 
 })();
