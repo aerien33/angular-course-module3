bc35877c510e6e4d8992f1952daa51fe04a851c0
Agnieszka <145773068+aerien33@users.noreply.github.com.>
2024-05-13 12:03:04 +0200

    Implement the functionality of service - add getMatchedMenuItems() method

--- scripts/app.js
+++ scripts/app.js
@@ -11,6 +11,24 @@ function NarrowItDownController() {
 
 function MenuSearchService() {
     var service = this;
+
+    service.getMatchedMenuItems = function (searchTerm) {
+        return $http({
+                   method: "GET",
+                   url: "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json"
+               }).then(function (result) {
+                   var foundItems = [];
+                   for (let category in result.data) {
+                       for (let item in category.menu_items) {
+                           if (item.description.toLowerCase().indexOf(searchTerm) !== -1) {
+                               foundItems.push(item);
+                           }
+                       }
+
+                   return foundItems;
+               }
+        });
+    }
 }
 
 })();
